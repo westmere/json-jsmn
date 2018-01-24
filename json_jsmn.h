040af454f89a12414834bf674bc69bfb26ec99df
vinh <vinh@debian9>
2018-01-24 15:29:05 +0700

    first commit

---
+++ json_jsmn.h
@@ -0,0 +1,16 @@
+#ifndef __JSON_JSMN_H_
+#define __JSON_JSMN_H_
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __JSON_JSMN_H_ */
